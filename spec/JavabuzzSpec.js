59565d74c0adde55c5a83498a5ed65c597de5d62
BecaLParker <becaparker@gmail.com>
2021-06-02 17:26:52 +0100

    ESLinted

--- spec/JavabuzzSpec.js
+++ spec/JavabuzzSpec.js
@@ -1,54 +1,53 @@
-describe('Javabuzz', function() {
+describe('Javabuzz', function () {
+  let javabuzz
 
-  var javabuzz;
+  beforeEach(function () {
+    javabuzz = new Javabuzz()
+  })
 
-  beforeEach(function() {
-    javabuzz = new Javabuzz();
-  });
+  describe('knows when a number is', function () {
+    it('divisible by 3', function () {
+      expect(javabuzz.isDivisibleByThree(3)).toBe(true)
+    })
 
-  describe('knows when a number is', function() {
-    it('divisible by 3', function() {
-      expect(javabuzz.isDivisibleByThree(3)).toBe(true);
-    });
+    it('divisible by 5', function () {
+      expect(javabuzz.isDivisibleByFive(5)).toBe(true)
+    })
 
-    it('divisible by 5', function() {
-      expect(javabuzz.isDivisibleByFive(5)).toBe(true);
-    });
+    it('divisible by 3 AND 5', function () {
+      expect(javabuzz.isDivisibleByFifteen(15)).toBe(true)
+    })
+  })
 
-    it('divisible by 3 AND 5', function() {
-      expect(javabuzz.isDivisibleByFifteen(15)).toBe(true);
-    });
-  });
+  describe('knows when a number is NOT', function () {
+    it('divisible by 3', function () {
+      expect(javabuzz.isDivisibleByThree(1)).toBe(false)
+    })
 
-  describe('knows when a number is NOT', function() {
-    it('divisible by 3', function() {
-      expect(javabuzz.isDivisibleByThree(1)).toBe(false);
-    });
+    it('divisible by 5', function () {
+      expect(javabuzz.isDivisibleByFive(1)).toBe(false)
+    })
 
-    it('divisible by 5', function() {
-      expect(javabuzz.isDivisibleByFive(1)).toBe(false);
-    });
+    it('divisible by 3 AND 5', function () {
+      expect(javabuzz.isDivisibleByFifteen(1)).toBe(false)
+    })
+  })
 
-    it('divisible by 3 AND 5', function() {
-      expect(javabuzz.isDivisibleByFifteen(1)).toBe(false);
-    });
-  });
+  describe('when playing, says', function () {
+    it('"Java" when a number is divisible by 3', function () {
+      expect(javabuzz.says(3)).toEqual('Java')
+    })
 
-  describe('when playing, says', function() {
-    it('"Java" when a number is divisible by 3', function() {
-      expect(javabuzz.says(3)).toEqual("Java");
-    });
+    it('"Buzz" when a number is divisible by 5', function () {
+      expect(javabuzz.says(5)).toEqual('Buzz')
+    })
 
-    it('"Buzz" when a number is divisible by 5', function() {
-      expect(javabuzz.says(5)).toEqual("Buzz");
-    });
+    it('"Javabuzz" when a number is divisible by 15', function () {
+      expect(javabuzz.says(15)).toEqual('Javabuzz')
+    })
 
-    it('"Javabuzz" when a number is divisible by 15', function() {
-      expect(javabuzz.says(15)).toEqual("Javabuzz");
-    });
-
-    it('the given number if not divisible by 3, 5 or 15', function() {
-      expect(javabuzz.says(1)).toEqual(1);
-    });
-  });
-});
+    it('the given number if not divisible by 3, 5 or 15', function () {
+      expect(javabuzz.says(1)).toEqual(1)
+    })
+  })
+})
--- src/Javabuzz.js
+++ src/Javabuzz.js
@@ -1,39 +1,38 @@
 class Javabuzz {
-  
-  isDivisibleByThree(number) {
-    return this._isDivisibleBy(number, 3);
+  isDivisibleByThree (number) {
+    return this._isDivisibleBy(number, 3)
   }
 
-  isDivisibleByFive(number) {
-    return this._isDivisibleBy(number, 5);
+  isDivisibleByFive (number) {
+    return this._isDivisibleBy(number, 5)
   }
 
-  isDivisibleByFifteen(number) {
-    return this._isDivisibleBy(number, 15);
+  isDivisibleByFifteen (number) {
+    return this._isDivisibleBy(number, 15)
   }
 
-  _isDivisibleBy(number, divisor) {
-    return (number % divisor === 0);
+  _isDivisibleBy (number, divisor) {
+    return (number % divisor === 0)
   }
 
-  says(number) {
+  says (number) {
     if (this.isDivisibleByFifteen(number)) {
-      return "Javabuzz";
+      return 'Javabuzz'
     }
 
     if (this.isDivisibleByThree(number)) {
-      return "Java";
+      return 'Java'
     }
 
     if (this.isDivisibleByFive(number)) {
-        return "Buzz";
+      return 'Buzz'
     }
-    return number;
+    return number
   }
 }
 
-var javaBuzz = new Javabuzz();
+const javaBuzz = new Javabuzz()
 
 for (let i = 1; i <= 100; i++) {
-  console.log(javaBuzz.says(i));
+  console.log(javaBuzz.says(i))
 }
